f7eb8e99782e39a96df8f977fd088aaad84fd61b
Ales Nezbeda <anezbeda@redhat.com>
2025-01-21 16:54:38 +0100

    el9to10: actors: mysql: Split report functions
    Split report functions so they are more coherent.
    Define list separator for standardized list look.

--- repos/system_upgrade/el9toel10/actors/mysqlcheck/libraries/mysqlcheck.py
+++ repos/system_upgrade/el9toel10/actors/mysqlcheck/libraries/mysqlcheck.py
@@ -5,6 +5,8 @@ from leapp.models import DistributionSignedRPM
 
 import subprocess
 
+FMT_LIST_SEPARATOR = '\n    - '
+
 
 class MySQLCheckLib:
     REMOVED_ARGS = [
@@ -21,31 +23,116 @@ class MySQLCheckLib:
         '--old-style-user-limits',
     ]
 
-    # Summary for mysql-server report
-    report_server_inst_summary = (
-        'MySQL server component will be upgraded.\n'
-        'Since RHEL-10 includes MySQL server 8.4 by default, it might be necessary'
-        ' to proceed with additional steps.'
-        '\n'
-    )
-
-    report_server_inst_hint = (
-        'Back up your data before proceeding with the upgrade'
-        ' and follow steps in the documentation section "Migrating to a RHEL 10 version of MySQL"'
-        ' after the upgrade.'
-    )
-
     # Link URL for mysql-server report
     report_server_inst_link_url = 'https://access.redhat.com/articles/7099234'
 
-    # Default title
-    report_title = 'MySQL (mysql-server) has been detected on your system'
-
-    # Default severity
-    report_severity = reporting.Severity.MEDIUM
-
     found_arguments = set()
     found_options = set()
+
+    def _generate_mysql_present_report(self):
+        """
+        Create report on mysql-server package installation detection.
+
+        Should remind user about present MySQL server package
+        installation, warn them about necessary additional steps, and
+        redirect them to online documentation for the upgrade process.
+
+        This report is used in case MySQL package is detected, but no
+        immediate action is needed.
+        """
+        reporting.create_report([
+            reporting.Title('Further action to upgrade MySQL might be needed'),
+            reporting.Summary((
+                'MySQL server component will be upgraded. '
+                'Since RHEL-10 includes MySQL server 8.4 by default, '
+                'it might be necessary to proceed with additional steps after '
+                'RHEL upgrade is completed. In simple setups MySQL server should '
+                'automatically upgrade all data on first start, but in more '
+                'complicated setups further steps might be needed.'
+            )),
+            reporting.Severity(self.report_severity),
+            reporting.Groups([reporting.Severity.LOW]),
+            reporting.ExternalLink(title='Migrating MySQL databases from RHEL 9 to RHEL 10',
+                                   url=self.report_server_inst_link_url),
+            reporting.RelatedResource('package', 'mysql-server'),
+            reporting.Remediation(hint=(
+                'Dump or backup your data before proceeding with the upgrade '
+                'and consult attached article '
+                '\'Migrating MySQL databases from RHEL 9 to RHEL 10\' '
+                'with up to date recommended steps before and after the upgrade - '
+                'especially different ways of backing up your data or steps you'
+                'may need to take manually.'
+            )),
+            ])
+
+    def _generate_deprecated_config_report(self):
+        """
+        Create report on mysql-server deprecated configuration.
+
+        Apart from showing user the article for upgrade process, we inform the
+        user that there are deprecated configuration options being used and
+        proceeding with upgrade will result in MySQL server failing to start.
+        """
+
+        generated_list = ''
+        if self.found_options:
+            generated_list += (
+                'Following configuration options won\'t work on a new version '
+                'of MySQL after upgrading and have to be removed from config files:'
+                )
+
+            for arg in self.found_options:
+                self.report_server_inst_summary += FMT_LIST_SEPARATOR + arg
+
+            generated_list += (
+                '\nDefault configuration file is present in `/etc/my.cnf`\n'
+            )
+
+        if self.found_arguments:
+            generated_list += (
+                'Following startup argument won\'t work on a new version '
+                'of MySQL after upgrading and have to be removed from '
+                'systemd service files:'
+                )
+
+            for arg in self.found_arguments:
+                self.report_server_inst_summary += FMT_LIST_SEPARATOR + arg
+
+            generated_list += (
+                '\nDefault service override file is present in '
+                '`/etc/systemd/system/mysqld.service.d/override.conf`'
+            )
+
+        reporting.create_report([
+            reporting.Title('MySQL is using configuration that will be invalid after upgrade'),
+            reporting.Summary((
+                'MySQL server component will be upgraded. '
+                'Since RHEL-10 includes MySQL server 8.4 by default, '
+                'it is necessary to proceed with additional steps. '
+                'Some options that are currently used in MySQL configuration are '
+                'deprecated and will result in MySQL server failing to start '
+                'after upgrading.'
+                'In simple setups MySQL server should automatically upgrade all '
+                'data on first start, after RHEL upgrade is completed. In more '
+                'complicated setups further steps might be needed.'
+            )),
+            reporting.Severity(self.report_severity),
+            reporting.Groups([reporting.Severity.MEDIUM]),
+            reporting.ExternalLink(title='Migrating MySQL databases from RHEL 9 to RHEL 10',
+                                   url=self.report_server_inst_link_url),
+            reporting.RelatedResource('package', 'mysql-server'),
+            reporting.Remediation(hint=(
+                'To ensure smooth upgrade process it is strongly recommended to '
+                'remove deprecated config options \n' +
+                generated_list +
+                'Dump or backup your data before proceeding with the upgrade '
+                'and consult attached article '
+                '\'Migrating MySQL databases from RHEL 9 to RHEL 10\' '
+                'with up to date recommended steps before and after the upgrade - '
+                'especially different ways of backing up your data or steps you'
+                'may need to take manually.'
+            )),
+            ])
 
     def _generate_report(self):
         """
@@ -57,31 +144,9 @@ class MySQLCheckLib:
         """
 
         if self.found_arguments or self.found_options:
-            self.report_severity = reporting.Severity.HIGH
-            self.report_title = 'MySQL (mysql-server) seems to be using deprecated config options'
-            self.report_server_inst_summary += (
-                '\nWarning:\n'
-                'It seems that some config options currently used for MySQL server'
-                ' will be removed in updated MySQL server.\n'
-                'If you proceed with the update now, without addressing this issue,'
-                ' the MySQL server will fail to start until the config is fixed.\n'
-                'Detected deprecated options:\n'
-            )
-            for arg in self.found_options:
-                self.report_server_inst_summary += f"{arg} in MySQL config file\n"
-            for arg in self.found_arguments:
-                self.report_server_inst_summary += f"{arg} in SystemD service override\n"
-
-        reporting.create_report([
-            reporting.Title(self.report_title),
-            reporting.Summary(self.report_server_inst_summary),
-            reporting.Severity(self.report_severity),
-            reporting.Groups([reporting.Groups.SERVICES]),
-            reporting.ExternalLink(title='Migrating to a RHEL 10 version of MySQL',
-                                   url=self.report_server_inst_link_url),
-            reporting.RelatedResource('package', 'mysql-server'),
-            reporting.Remediation(hint=self.report_server_inst_hint),
-            ])
+            self._generate_deprecated_config_report()
+        else:
+            self._generate_mysql_present_report()
 
     def _check_incompatible_config(self):
         # mysqld --validate-config --log-error-verbosity=2
